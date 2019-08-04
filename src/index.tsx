a87e8d6f85920c0f497dbba4cb7fcba7699407f1
Nick Middleweek <nick@middleweek.co.uk>
2019-08-04 20:34:39 +0100

    Switch to use position absolute for label

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="metric">
-        <div className="metric__label">Metric label</div>
+        <div className="metric__label">Component label</div>
         <div className="metric__wrapper">
           <div className="metric__value">8</div>
           <div className="metric__sub-value">88</div>
